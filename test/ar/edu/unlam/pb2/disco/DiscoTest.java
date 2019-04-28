33a5435cb674f7401125f6db7e38da05a813a4be
aagustin_7 <agus.arana7@gmail.com>
2019-04-28 14:28:32 -0300

    Se finalizo la clase DiscoTest.java

--- test/ar/edu/unlam/pb2/disco/DiscoTest.java
+++ test/ar/edu/unlam/pb2/disco/DiscoTest.java
@@ -23,11 +23,6 @@ public class DiscoTest {
 		double superficieEsperada = 12.56;
 		
 		//3.contrastación.
-		System.out.println("El disco creado tiene las siguientes características:\n\r" + 
-		"Radio Interior:\t\t\t" + radiointerior + "\r" + "Radio Exterior:\t\t\t" + radioexterior + 
-		"\r\r" + "El perimetro Interior es:\t" + perimetroInterior + "\r" +"El perimetro Exterior es:\t" 
-		+ perimetroExterior + "\r" + "Y la superficie es:\t\t" + superficie);
-		
 		Assert.assertEquals(true, perimetroInterior == perimetroInteriorEsperado);
 		Assert.assertEquals(true, perimetroExterior == perimetroExteriorEsperado);
 		Assert.assertEquals(true, superficie == superficieEsperada);
